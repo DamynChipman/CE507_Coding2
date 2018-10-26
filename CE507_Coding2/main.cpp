28169cfc0bf30c9fb403237624fd460ff66d0424
Damyn Chipman <damyn.chipman@gmail.com>
2018-10-26 11:39:55 -0600

    Initial Commit

---
+++ CE507_Coding2/main.cpp
@@ -0,0 +1,13 @@
+//   Created by Damyn Chipman on 10/26/18
+//   github: @camperD
+//   Copyright © 2018 Damyn Chipman. All rights reserved.
+//      FILE:   main.cpp
+//   PROJECT:   CE507_Coding2
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
